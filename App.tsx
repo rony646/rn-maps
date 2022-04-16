5cd5bbee1c024a8ab3d9e7ae2afb7745bbf78e88
Rafael <rteles78@gmail.com>
2022-04-16 12:06:20 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,62 @@
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+interface propsCoords {
+  latitude: number
+  longitude: number
+  description: string
+}
+
+const initialProps = {
+  latitude: -22.0905194,
+  longitude: -51.3729107,    
+  description: ''
+}
 
 export default function App() {
+  const [dataPointA, setDataPointA] = useState<propsCoords>(initialProps)
+  const [dataPointB, setDataPointB] = useState<propsCoords>(initialProps)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style='auto'/>
+      
+      <View style={styles.footer}>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  footer: {
+    backgroundColor: '#f2f2f2',
+  },
+  map: {   
+    flex: 1, 
+    backgroundColor: '#f2f2f2',
     alignItems: 'center',
     justifyContent: 'center',
+  },  
+  button: {
+    backgroundColor: 'green',
+    margin: 10,
+    borderRadius: 50,    
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 18,
+    color: '#f2f2f2'
+  },
+  informations: {
+    paddingHorizontal: 20,    
+    marginBottom: 12,
+    marginHorizontal: 20,
+    backgroundColor: '#c9c9c9',
+    borderRadius: 50,
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 50,    
   },
 });
