2e6610093099064c15d3aa7f8da26a937af65ce4
Rony Silva <rony.silva@grupoboticario.com.br>
2022-06-03 00:01:17 -0300

    feat: adding markers and maps

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { useRef, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GOOGLE_MAPS_APIKEY } from '@env';
+import MapView, { Marker } from 'react-native-maps';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MapViewDirections from 'react-native-maps-directions';
-import mapStyle from './mapStyle';
+import mapStyle from './mapStyled';
 
 interface propsCoords {
   latitude: number
@@ -24,21 +24,21 @@ export default function App() {
   const [dataPointB, setDataPointB] = useState<propsCoords>(initialProps)
   const mapRef = useRef<any>(null);
 
-  const onLoayoutMap = () => {
-    if (!dataPointA.description || !dataPointB.description) return;
+  const onLayoutMap = () => {
+    if(!dataPointA.description || !dataPointB.description) return;
 
     mapRef.current.fitToSuppliedMarkers(['pointA', 'pointB'], {
       edgePadding: { top: 50, right: 50, bottom: 50, left: 50 },
     })
-  }
+  };
 
   return (   
     <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style='auto'/>      
 
-      <MapView 
+      <MapView
         ref={mapRef}
-        style={styles.map}
+        style={styles.map} 
         mapType="mutedStandard"
         initialRegion={{
           latitude: dataPointA.latitude,
@@ -47,19 +47,19 @@ export default function App() {
           longitudeDelta: 0.005,
         }}
         zoomControlEnabled
-        scrollEnabled  
         minZoomLevel={5}
-        maxZoomLevel={15}  
-        onLayout={onLoayoutMap}  
-        customMapStyle={mapStyle}  
-      > 
+        maxZoomLevel={15}
+        scrollEnabled
+        onLayout={onLayoutMap}
+        customMapStyle={mapStyle}
+      >
 
         <MapViewDirections
           origin={dataPointA.description}
           destination={dataPointB.description}
           apikey={GOOGLE_MAPS_APIKEY}
           strokeWidth={6}
-          strokeColor="blue"
+          strokeColor="black"
         />
 
         <Marker
@@ -67,98 +67,92 @@ export default function App() {
           title="Ponto A"
           description={dataPointA.description}
           identifier="pointA"
-        >          
-          <Image style={styles.avatar}
-            source={{ uri: 'https://avatars.githubusercontent.com/u/53085758?v=4' }}/>         
+        >
+          <Image source={{uri: 'https://avatars.githubusercontent.com/u/60190948?v=4' }} style={styles.avatar}/>
         </Marker>
 
         <Marker
           coordinate={dataPointB}
           title="Ponto B"
           description={dataPointB.description}
-          identifier="pointB"
+          identifier='pointB'
         >
-          <Image style={styles.avatar}
-            source={{ uri: 'https://avatars.githubusercontent.com/u/53085758?v=4' }}/>
+          <Image source={{uri: 'https://avatars.githubusercontent.com/u/60190948?v=4' }} style={styles.avatar}/>
         </Marker>
       </MapView>
       
       <View style={styles.footer}>
+          <GooglePlacesAutocomplete
+            placeholder='Ponto A'
+            fetchDetails
+            enablePoweredByContainer={false}
+            minLength={3}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            onPress={(data, details = null) => {
+              setDataPointA({
+                latitude: details?.geometry.location.lat || 0,
+                longitude: details?.geometry.location.lng || 0,
+                description: data.description,
+              })
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'pt-BR',
+            }}
+            styles={{
+              container: {
+                flex: 0,
+                margin: 20,
+                shadowOpacity: 0.75,
+                shadowRadius: 5,
+                shadowColor: 'blue',
+                shadowOffset: {
+                  height: 10,
+                  width: 10,
+                },
+                borderColor: '#242F3E',
+                borderRadius: 10,
+                borderWidth: 1,
+              }
+            }}
+          />
 
         <GooglePlacesAutocomplete
-          placeholder="Ponto a"
-          styles={{
-            container: {
-              flex: 0,
-              margin: 20,
-              shadowOpacity: 0.75,
-              shadowRadius: 5,
-              shadowColor: 'blue',
-              shadowOffset: { height: 10, width: 10 },
-              borderColor: '#242f3e',
-              borderRadius: 10,
-              borderWidth: 1
-            },
-            textInput: {
-              fontSize: 14,
-              borderRadius: 10,
-            },
-          }}
-          fetchDetails
-          onPress={(data, details = null) => {
-            setDataPointA({
-              latitude: details?.geometry.location.lat || 0,
-              longitude: details?.geometry.location.lng || 0,
-              description: data.description
-            })
-          }}
-          enablePoweredByContainer={false}
-          minLength={3}
-          query={{
-            key: GOOGLE_MAPS_APIKEY,
-            language: "pt-BR",
-          }}
-          nearbyPlacesAPI="GooglePlacesSearch"
-          debounce={400}
-        />
-
-        <GooglePlacesAutocomplete
-          placeholder="Ponto b"
-          styles={{
-            container: {
-              flex: 0,
-              margin: 20,
-              shadowOpacity: 0.75,
-              shadowRadius: 5,
-              shadowColor: 'blue',
-              shadowOffset: { height: 10, width: 10 },
-              borderColor: '#242f3e',
-              borderRadius: 10,
-              borderWidth: 1
-            },
-            textInput: {
-              fontSize: 14,
-              borderRadius: 10,
-            },
-          }}
-          fetchDetails
-          onPress={(data, details = null) => {
-            setDataPointB({
-              latitude: details?.geometry.location.lat || 0,
-              longitude: details?.geometry.location.lng || 0,
-              description: data.description
-            })
-          }}
-          enablePoweredByContainer={false}
-          minLength={3}
-          query={{
-            key: GOOGLE_MAPS_APIKEY,
-            language: "pt-BR",
-          }}
-          nearbyPlacesAPI="GooglePlacesSearch"
-          debounce={400}
-        />
-              
+            placeholder='Ponto B'
+            fetchDetails
+            enablePoweredByContainer={false}
+            minLength={3}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+            onPress={(data, details = null) => {
+              setDataPointB({
+                latitude: details?.geometry.location.lat || 0,
+                longitude: details?.geometry.location.lng || 0,
+                description: data.description,
+              })
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: 'pt-BR',
+            }}
+            styles={{
+              container: {
+                flex: 0,
+                margin: 20,
+                shadowOpacity: 0.75,
+                shadowRadius: 5,
+                shadowColor: 'blue',
+                shadowOffset: {
+                  height: 10,
+                  width: 10,
+                },
+                borderColor: '#242F3E',
+                borderRadius: 10,
+                borderWidth: 1,
+              }
+            }}
+          />
       </View>
     </SafeAreaView>
   );
